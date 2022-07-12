468b4923c1da702ea4f1b71d77fcce5df1f36738
Domenic Denicola <d@domenic.me>
2022-07-12 17:48:10 +0900

    Add data-x="" to the output

--- js/generator.mjs
+++ js/generator.mjs
@@ -1,6 +1,7 @@
 import createDiagram from "./create-diagram.mjs";
 
 const textarea = document.querySelector("textarea");
+const htmlSpecMode = document.querySelector("#html-spec-mode");
 const preview = document.querySelector("#preview");
 const code = document.querySelector("#code");
 const permalink = document.querySelector("#permalink");
@@ -9,11 +10,22 @@ if (location.hash) {
   textarea.value = atob(location.hash.substring(1));
 }
 
-textarea.oninput = () => {
+textarea.oninput = htmlSpecMode.oninput = update;
+update();
+
+permalink.onclick = () => {
+  location.href = getPermalink(textarea.value);
+};
+
+
+function update() {
   const input = textarea.value;
 
   try {
     const table = createDiagram(input);
+    if (htmlSpecMode.checked) {
+      addDataX(table);
+    }
     const comment = `<!--${getPermalink(input)} -->`;
 
     preview.replaceChildren(table);
@@ -22,15 +34,16 @@ textarea.oninput = () => {
     preview.innerHTML = `<p class="error">${e.message}</p><p class="stack">${e.stack}</p>`;
     code.replaceChildren();
   }
-};
-textarea.oninput();
-
-permalink.onclick = () => {
-  location.href = getPermalink(textarea.value);
-};
+}
 
 function getPermalink(input) {
   const url = new URL(location.href);
   url.hash = btoa(input);
   return url.href;
+}
+
+function addDataX(rootElement) {
+  for (const code of rootElement.querySelectorAll("code")) {
+    code.setAttribute("data-x", "");
+  }
 }
